766360b4ae9ab476724cbf2bfc4221a37a8705c8
Benn McGregor <benn@Benns-MacBook-Pro.local>
2020-04-13 01:10:15 -0400

    test

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -1,11 +1,11 @@
 const ghpages = require('gh-pages')
 
-// replace with your repo url
 ghpages.publish(
   'public',
   {
     branch: 'master',
     repo: 'https://github.com/bennmcgregor/bennmcgregor.github.io',
+    add: true,
   },
   () => {
     console.log('Deploy Complete!')
