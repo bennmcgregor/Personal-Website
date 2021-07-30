bfd6acdba22c375f4cacbfc3b56138fdc306f296
Benn McGregor <benn@MacBook-Pro.local>
2021-07-30 15:03:38 -0400

    changed deployment script

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -4,7 +4,7 @@ ghpages.publish(
   'public',
   {
     branch: 'master',
-    repo: 'https://github.com/bennmcgregor/bennmcgregor.github.io',
+    repo: 'git@github.com:bennmcgregor/bennmcgregor.github.io.git',//'https://github.com/bennmcgregor/bennmcgregor.github.io',
     message: 'Auto-generated commit, please see https://github.com/bennmcgregor/Personal-Website for source code.'
   },
   () => {
